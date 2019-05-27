5d1f810df4b30f39c5076f601806d88370d30a04
yuta24 <yuta24@bivre.com>
2019-05-27 19:34:05 +0900

    Initialize project

---
+++ Lottie-playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lottie-playground
+//
+//  Created by Yu Tawata on 2019/05/27.
+//  Copyright © 2019 Yu Tawata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
